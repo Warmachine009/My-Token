6ce4c56dde0d37ffaceda4fc2d46ca6fa8a6ca1b
Warmachine009 <108185605+Warmachine009@users.noreply.github.com>
2025-07-30 12:27:39 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,19 @@
+/*const firstName = 'Aashith'
+const age = 2
+const final = `I'm ${firstName} and my age is ${age * 2}`
+console.log(final)
+const adult = age > 18
+if (adult) {
+    console.log(`${firstName} is an adult is ${adult}`);
+} else {
+    console.log(`${firstName} is an adult is ${adult}`);
+}
+console.log(String(age) + 11)
+console.log(Number(final) - 1)
+if (age === '2') {
+    console.log(`Type conversion`);
+}
+*/
+const hasDrivingLicense = true;
+const hasGoodVision = false;
+console.log(hasDrivingLicense || hasGoodVision);
